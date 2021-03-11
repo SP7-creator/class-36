00506a3e1b50ae6e8f2f3cc9b62c40269a6a6940
SP7-creator <76457031+SP7-creator@users.noreply.github.com>
2021-03-11 18:50:37 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var database;
+var canvas;
+var playerCount;
+var gameState = 0;
+var form, player, game;
+
+
+
+function setup(){
+  database = firebase.database();
+  
+  createCanvas(400,400);
+  game = new Game()
+  game.getState()
+  game.start()
+
+  
+
+
+ 
+}
+
+function draw(){
+  
+
+
+}
